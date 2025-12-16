d9b3bc8b697f564861d3f895e7e99818e411dbe7
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-16 23:58:29 +0530

    add DBConnection class for database connectivity

---
+++ src/main/java/org/example/dbConnection/DBConnection.java
@@ -0,0 +1,26 @@
+package org.example.dbConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+        private static DBConnection instance;
+        private Connection connection;
+
+        public DBConnection() throws SQLException {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/TO_DO_Application","root","1234");
+        }
+
+        public static DBConnection getInstance() throws SQLException {
+            if (instance == null){
+                instance = new DBConnection();
+            }
+            return instance;
+        }
+        public Connection getConnection(){
+            return connection;
+        }
+
+}
