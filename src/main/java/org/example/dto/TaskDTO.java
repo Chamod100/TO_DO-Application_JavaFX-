bb2c409bdd6c469c89e87f16ab2eac7becabca6d
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-17 17:15:07 +0530

    add TaskDTO class for task data transfer object

---
+++ src/main/java/org/example/dto/TaskDTO.java
@@ -0,0 +1,15 @@
+package org.example.dto;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class TaskDTO {
+    private int id;
+    private String title;
+    private String date;
+    private String description;
+}
